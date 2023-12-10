410742a7099cfa0d54fe1864f4e06d4f44c6bf91
Jenna Salhab <salhabje@msu.edu>
2023-12-09 22:01:29 -0500

    new layout

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add JavaScript below
+document.addEventListener('DOMContentLoaded', function() {
+    var filterForm = document.getElementById('filter-form');
+    if (filterForm) {
+        filterForm.addEventListener('submit', function(event) {
+            event.preventDefault();
+            applyFilters();
+        });
+    }
+});
+
+function applyFilters() {
+    var selectedSize = document.getElementById('size').value;
+    var products = document.getElementsByClassName('product-card');
+
+    for (var i = 0; i < products.length; i++) {
+        if (products[i].getAttribute('data-size') === selectedSize) {
+            products[i].style.display = ''; // Show product
+        } else {
+            products[i].style.display = 'none'; // Hide product
+        }
+    }
+}
+
