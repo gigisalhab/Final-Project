d7387c681c5a888f50b2016924acea533e8cf9ed
Jenna Salhab <salhabje@msu.edu>
2023-12-10 21:06:47 -0500

    products

--- index.js
+++ index.js
@@ -10,14 +10,33 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function applyFilters() {
     var selectedSize = document.getElementById('size').value;
+    var selectedPrice = document.getElementById('price').value;
     var products = document.getElementsByClassName('product-card');
 
     for (var i = 0; i < products.length; i++) {
-        if (products[i].getAttribute('data-size') === selectedSize) {
-            products[i].style.display = ''; // Show product
+        var productSize = products[i].getAttribute('data-size');
+        var productPrice = products[i].getAttribute('data-price');
+
+        if (productSize === selectedSize && productPrice === selectedPrice) {
+            products[i].style.display = '';
         } else {
-            products[i].style.display = 'none'; // Hide product
+            products[i].style.display = 'none';
         }
     }
 }
 
+
+document.addEventListener('DOMContentLoaded', function() {
+    let currentImageIndex = 0;
+    const imagesContainer = document.querySelector('.carousel-images'); // Container of the images
+    const totalImages = document.querySelectorAll('.product-carousel img').length;
+
+    function moveCarousel(step) {
+        currentImageIndex = (currentImageIndex + step + totalImages) % totalImages;
+        const offset = -currentImageIndex * 100;
+        imagesContainer.style.transform = `translateX(${offset}%)`;
+    }
+
+    document.getElementById('left-arrow').addEventListener('click', () => moveCarousel(-1));
+    document.getElementById('right-arrow').addEventListener('click', () => moveCarousel(1));
+});
